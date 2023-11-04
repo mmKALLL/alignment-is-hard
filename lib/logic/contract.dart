25b64656b5c6408497efb4b069e3f97f05aaf6b6
Esa Koskinen <esakoskin2@gmail.com>
2023-11-04 21:21:05 +0900

    tweak: rebalance alignment requirement for contracts
    - this allows 5 req contracts to be split 3/2 instead of 4/1. Overall the growth is closer to intended now

--- lib/logic/contract.dart
+++ lib/logic/contract.dart
@@ -76,7 +76,7 @@ Contract getRandomContract(GameState gs) {
 
   // Requirements rise exponentially with difficulty
   final int totalRequirement = pow(((100 + difficulty) / 100), 1.7).round();
-  final int alignmentRequirement = totalRequirement >= 2 && isAlignmentContract ? (totalRequirement * 0.7).round() : 0;
+  final int alignmentRequirement = totalRequirement >= 2 && isAlignmentContract ? (totalRequirement * 0.64).round() : 0;
   final List<ActionEffect> requirements = [
     if (alignmentRequirement > 0) ActionEffect(Param.rp, -alignmentRequirement.toDouble()),
     ActionEffect(Param.ep, -(totalRequirement - alignmentRequirement).toDouble())
